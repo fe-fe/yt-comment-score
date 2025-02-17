b0dce8089890ae5fabdf392171c539373737905a
fefe <119269054+fe-fe@users.noreply.github.com>
2025-02-17 00:49:12 -0300

    organizacao do codigo

---
+++ nlp.py
@@ -0,0 +1,62 @@
+from dotenv import load_dotenv
+from google import genai
+import os
+from nltk.sentiment import SentimentIntensityAnalyzer
+from statistics import fmean
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+import string
+
+swp = stopwords.words("portuguese")
+swp.extend(string.punctuation)
+
+sia = SentimentIntensityAnalyzer()
+
+load_dotenv()
+
+GEMINIKEY = os.getenv("GEMINIKEY")
+
+client = genai.Client(api_key=GEMINIKEY)
+
+
+def translate(sample: list) -> list:
+    # ";;;;;;" eh o delimitador dos comentarios
+    prompt = ";;;;;;".join(sample)
+    prompt = """Traduza para ingles o seguinte prompt e mantenha os comentarios 
+    separados por ';;;;;;'. Responda apenas o texto traduzido, nada mais:"""+prompt
+
+    # traduz a amostra de comentarios para ingles utilizando a Gemini AI.
+    # Textos em ingles sao processados com mais precisao pelo modulo de sentimentos.
+    traduzido = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=prompt
+    )
+
+    sample_ingles = traduzido.text.split(";;;;;;")
+    return sample_ingles
+
+
+def get_polarity(sample: list) -> list:
+    polarity = []
+    for s in sample:
+        p = sia.polarity_scores(sample[s]) # processa os sentimentos do texto
+        polarity.append(p["compound"])
+
+    media = (fmean(polarity)+1)*50 # faz a media das polaridades e converte em porcentagem, variando entre 0 e 100%
+    return polarity, media
+
+
+def get_common_words(sample: list) -> list:
+    
+    sample = ".".join(sample) # une a sample em uma string separando os indices por pontos
+    tokens = word_tokenize(sample.lower()) # extrai os tokens da string
+
+    # filtra os tokens por palavras importantes
+    filtrada = []
+    for t in tokens:
+        if t not in swp:
+            filtrada.append(t)
+    
+    freq = FreqDist(filtrada) # extrai as palavras mais comuns
+    return freq.most_common(5) # retorna as 5 palavras mais frequentes
--- scraper.py
+++ scraper.py
@@ -4,28 +4,6 @@ from selenium.common.exceptions import ElementNotInteractableException
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from dotenv import load_dotenv
-from google import genai
-import os
-from nltk.sentiment import SentimentIntensityAnalyzer
-import nltk
-from statistics import fmean
-from nltk.tokenize import word_tokenize
-from nltk.probability import FreqDist
-from nltk.corpus import stopwords
-import string
-
-swp = stopwords.words("portuguese")
-swp.extend(string.punctuation)
-
-sia = SentimentIntensityAnalyzer()
-
-load_dotenv()
-
-GEMINIKEY = os.getenv("GEMINIKEY")
-
-client = genai.Client(api_key=GEMINIKEY)
-
 
 browser = webdriver.Chrome()
 
@@ -36,7 +14,20 @@ link = "https://www.youtube.com/watch?v=S-2fw-UOcbw"
 def get_comment_sample(link: str, qtd: int, browser: webdriver.Chrome, exceed=True) -> list:
     sample = []
     browser.get(link)
-    sleep(1)
+    sleep(1) # espera o conteudo da pagina ser carregado
+
+    """
+    enquanto a quantidade desejada de comentarios para amostra nao for alcancada,
+    scrollar a pagina para baixo e procurar por mais comentarios
+
+    o elemento do comentario pode ser identificado pelo id do seu elemento pai 'content-text'
+
+    Os comentarios podem nao estar disponiveis de primeira mao, 
+    portanto tratamos a execao ElementNotInteractableException com continue
+    exceed diz se a quantidade de comentarios da sample pode ser excedida ou nao
+    Ex: caso  qtd = 25 exceed=true, a funcao pode retornar 20 ou mais comentarios, geralmente variando de 1 a 10
+    """
+
     while len(sample) < qtd:
         try:
             ActionChains(browser)\
@@ -58,54 +49,3 @@ def get_comment_sample(link: str, qtd: int, browser: webdriver.Chrome, exceed=Tr
         sample = sample[0:qtd]
     return sample
 
-
-sample = get_comment_sample(link, 20, browser, False)
-
-prompt = ";;;;;;".join(sample)
-
-prompt = "Traduza para ingles o seguinte prompt e mantenha os comentarios separados por ';;;;;;'. Responda apenas o texto traduzido, nada mais:"+prompt
-
-traduzido = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents=prompt
-)
-
-sample_ingles = traduzido.text.split(";;;;;;")
-
-
-i = 0
-sentiments = []
-for s in sample:
-    if len(s) > 100:
-        print(s[0:100]+"...")
-    else:
-        print(s) 
-    
-    sentiment = sia.polarity_scores(sample_ingles[i])
-    sentiments.append(sentiment["compound"])
-    print(f"Result: {round((sentiment["compound"]+1)*50, 2)}%")
-    print("-"*50)
-    i+=1
-
-media = (fmean(sentiments)+1)*50
-if media >= 50:
-    result = "POSITIVA"
-else:
-    result = "NEGATIVA"
-
-print(f"media final: {media}% ({result})")
-print("-"*50)
-
-
-
-sample = ".".join(sample)
-tokens = word_tokenize(sample.lower())
-
-filtrada = []
-for t in tokens:
-    if t not in swp:
-        filtrada.append(t)
-
-freq = FreqDist(filtrada)
-
-print(freq.most_common(30))
